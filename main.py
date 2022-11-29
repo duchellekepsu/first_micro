8afc03f326ccd0cb4373c59f43769313539668a5
duchellekepsu <sylvanieduchel.kepsutchahou@epfedu.fr>
2022-11-29 16:47:59 +0100

    second commit

--- main.py
+++ main.py
@@ -4,5 +4,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async 
+src="https://www.googletagmanager.com/gtag/js?id=G-62KZF33BEW"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-62KZF33BEW');
+</script>
+ """
+ return prefix_google + "Hello World"
+
 
